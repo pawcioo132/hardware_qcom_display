2577bf453d111d138a3f60c5802d54050365f72b
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-05-03 23:40:36 -0400

    common.mk we should use logic with DNO_IOMMU

--- common.mk
+++ common.mk
@@ -57,6 +57,6 @@ ifneq ($(TARGET_DISPLAY_INSECURE_MM_HEAP),true)
     common_flags += -DSECURE_MM_HEAP
 endif
 
-ifneq ($(filter msm8660 msm7x30 msm7x27a,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     common_flags += -DNO_IOMMU
 endif
