8e1d43842d5a8bdbb3e534b33f874d3a8e98911b
Jeykumar Sankaran <jsanka@codeaurora.org>
2015-05-14 11:54:58 -0700

    Revert "hwc: Skip load based composition for video playback."
    Reason for revert: Not needed in high end targets since we have
    more than one DMA pipes with DMA multiplexing enabled.
    
    This reverts commit 3dbbd8853ab537afaf5487c962f88d077792702d.
    
    Conflicts:
    	libhwcomposer/hwc_mdpcomp.cpp
    
    Change-Id: I03f143d2c1904de500cac7df4bbbeb81ff387f2d

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -1347,8 +1347,7 @@ bool MDPComp::loadBasedComp(hwc_context_t *ctx,
 }
 
 bool MDPComp::isLoadBasedCompDoable(hwc_context_t *ctx) {
-    if(mDpy or isSecurePresent(ctx, mDpy) or
-            isYuvPresent(ctx, mDpy)) {
+    if(mDpy or isSecurePresent(ctx, mDpy)) {
         return false;
     }
     return true;
