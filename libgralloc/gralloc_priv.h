CyanogenMod/android_hardware_qcom_display

gralloc: Add a private flag for INTERNAL only
Add PRIV_FLAGS_INTERNAL_ONLY which will be used by clients
to know if that is an INTERNAL only layer.

Change-Id: I2cc69bcbc86c18d97ce0c841670b760e48721ffa

libgralloc/gralloc_priv.h
@@ -181,6 +181,7 @@ struct private_handle_t : public native_handle {
             PRIV_FLAGS_USES_ION           = 0x00000008,
             PRIV_FLAGS_USES_ASHMEM        = 0x00000010,
             PRIV_FLAGS_NEEDS_FLUSH        = 0x00000020,
+            PRIV_FLAGS_INTERNAL_ONLY      = 0x00000040,
             PRIV_FLAGS_NON_CPU_WRITER     = 0x00000080,
             PRIV_FLAGS_NONCONTIGUOUS_MEM  = 0x00000100,
             PRIV_FLAGS_CACHED             = 0x00000200,

pawcioo132/hardware_qcom_display

libgralloc/gralloc_priv.h
@@ -158,6 +158,7 @@ struct private_handle_t : public native_handle {
             PRIV_FLAGS_EXTERNAL_ONLY      = 0x00002000,
             // Set by HWC for protected non secure buffers
             PRIV_FLAGS_PROTECTED_BUFFER   = 0x00004000,
+            PRIV_FLAGS_INTERNAL_ONLY      = 0x00008000,
             PRIV_FLAGS_VIDEO_ENCODER      = 0x00010000,
             PRIV_FLAGS_CAMERA_WRITE       = 0x00020000,
             PRIV_FLAGS_CAMERA_READ        = 0x00040000,